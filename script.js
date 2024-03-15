5fa33953e8def21908a24e57be3d1f4d897f18bb
Vanshika Sabharwal <vanshikasabharwal06@gmail.com>
2024-03-16 00:01:21 +0530

    added dark mode

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ cross.addEventListener("click", () => {
 const footer = document.querySelector(".footer");
 let year = new Date().getFullYear();
 footer.innerHTML = `Copyright &copy; ${year} by Vanshika Sabharwal | All Rights Reserved.`;
+
+// dark mode
+const icon = document.getElementById("icon");
+const body = document.querySelector("body");
+
+icon.addEventListener("click", function () {
+  this.classList.toggle("bi-brightness-high-fill");
+  if (this.classList.toggle("bi-moon")) {
+    body.style.background = "#1b1b1b";
+    icon.style.color = "white";
+    body.style.transition = "2s";
+  } else {
+    body.style.background = "white";
+    icon.style.color = "black";
+    body.style.transition = "2s";
+  }
+});
